b75e495dd3e624335fca83df2cf226f5beb6c9a7
sajiniekavindya <sajinie.14@cse.mrt.ac.lk>
2019-04-04 11:28:56 +0530

    Delete synapse extension from lsp4xml folder

--- extensions/org.eclipse.lsp4xml.extensions/src/main/java/org/eclipse/lsp4xml/extensions/contentmodel/namespaceResolver/SynapseNamespaceResolver.java
+++
@@ -1,6 +0,0 @@
-package org.eclipse.lsp4xml.extensions.contentmodel.namespaceResolver;
-
-public class SynapseNamespaceResolver {
-
-    public static final String SYNAPSE_NAMESPACE = "http://ws.apache.org/ns/synapse";
-}
